559121670ac26e7182bfee59628e28f4c771b69b
Matilda Mared <matildamared@live.se>
2024-04-29 07:43:30 +0200

    remove log

--- test-project/src/app/page.tsx
+++ test-project/src/app/page.tsx
@@ -2,7 +2,6 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
-	console.log("Hej!");
 	return (
 		<main className={styles.main}>
 			<h1>Home page</h1>
